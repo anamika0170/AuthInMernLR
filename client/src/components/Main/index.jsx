aca994d7553cbf47b72fcf767c4a517550fcb84b
“Shivani <shivirajput2408p@gmail.com>
2022-04-23 11:47:44 +0530

    login registration

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>Welcome To Home Page</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+			</nav>
+		</div>
+	);
+};
+
+export default Main;
